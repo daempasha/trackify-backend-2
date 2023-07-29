41832b39676e11e64cd0d217e8ab0c87f1eaacb1
Bilaal <mohammad.bilaal1@nhs.net>
2023-07-29 21:10:02 +0100

    add loggin

--- src/index.ts
+++ src/index.ts
@@ -13,22 +13,27 @@ import path from 'path';
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORTt;
+const port = process.env.PORT;
 
 const mongodb_uri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/trackify";
-const ca = fs.readFileSync(path.join(__dirname, '../global-bundle.pem'), 'utf8');
+let ca: string;
+try {
+  ca = fs.readFileSync(path.resolve(__dirname, '../global-bundle.pem'), 'utf8');
+} catch (err) {
+  console.error('Error reading certificate file:', err);
+  process.exit(1);
+}
 
 const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    ssl: true,
-    sslValidate: true,
-    sslCA: ca,
-    sslKey: ca,
-    sslCert: ca,
-    retryWrites: false
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  ssl: true,
+  sslValidate: true,
+  sslCA: [ca],
+  sslKey: ca,
+  sslCert: ca,
+  retryWrites: false,
 };
-
 const client = new MongoClient(mongodb_uri, options);
 
 client.connect()
