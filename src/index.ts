c6efa3046e7aa451cc014e98a65164280e825037
Bilaal <mohammad.bilaal1@nhs.net>
2023-07-30 03:15:45 +0100

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,50 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
 import { MongoClient } from 'mongodb';
 import palletRouter from "./routes/pallets";
 import itemsRouter from "./routes/items";
 import ebayRouter from "./routes/ebay";
 import cors from "cors";
 import morgan from "morgan";
-import fs from 'fs';
 import path from 'path';
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
-const base = process.env.PWD;
+const port = process.env.PORT || 3000;
 
 const mongodb_uri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/trackify";
-const ca = fs.readFileSync((base + '/global-bundle.pem'), 'utf8');
 
-const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    ssl: true,
-    sslValidate: true,
-    sslCA: ca,
-    sslKey: ca,
-    sslCert: ca,
+const client = new MongoClient(mongodb_uri, {
+    tlsCAFile: path.resolve(__dirname, '../global-bundle.pem'),
     retryWrites: false
-};
+});
 
-const client = new MongoClient(mongodb_uri, options);
-
-client.connect()
-    .then(() => {
+const main = async () => {
+    try {
+        await client.connect();
         console.log("Connected!");
-        mongoose.connection.useDb('trackify');
-    })
-    .catch(err => console.error(err));
+        const db = client.db('trackify');
 
-app.use(morgan("tiny"));
-app.use(
-  express.urlencoded({
-    extended: true,
-  }),
-);
-app.use(express.json());
-app.use(cors());
+        app.use(morgan("tiny"));
+        app.use(express.urlencoded({ extended: true }));
+        app.use(express.json());
+        app.use(cors());
 
-app.use("/api/pallets", palletRouter);
-app.use("/api/items", itemsRouter);
-app.use("/api/ebay", ebayRouter);
+        app.use("/api/pallets", palletRouter);
+        app.use("/api/items", itemsRouter);
+        app.use("/api/ebay", ebayRouter);
 
-app.get("/test", (req: Request, res: Response) => {
-  res.send({ message: "Hello world" });
-});
+        app.get("/test", (req: Request, res: Response) => {
+            res.send({ message: "Hello world" });
+        });
 
-app.listen(port, () => {
-  console.log(` ⚡️[server]: Server is running at http://localhost:${port}`);
-});
+        app.listen(port, () => {
+            console.log(`⚡️[server]: Server is running at http://127.0.0.1:${port}`);
+        });
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+main().catch(console.error);
